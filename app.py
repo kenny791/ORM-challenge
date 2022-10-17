ce01a15a9fcb5fbcf3806a9ef9769f7b4abaab78
Kenny Lai <kennylai@Kennys-MacBook-Pro.local>
2022-10-17 23:53:02 +1100

    added new route for registering new users

--- app.py
+++ app.py
@@ -135,3 +135,20 @@ def get_cards():
     result = cards_schema.dump(cards_list)
     # return the data in JSON format
     return jsonify(result)
+
+
+@app.route("/auth/register", methods=["POST"])
+def auth_register():
+    #The request data will be loaded in a user_schema converted to JSON. request needs to be imported from
+    user_fields = user_schema.load(request.json)
+    #Create the user object
+    user = User()
+    #Add the email attribute
+    user.email = user_fields["email"]
+    #Add the password attribute hashed by bcrypt
+    user.password = bcrypt.generate_password_hash(user_fields["password"]).decode("utf-8")
+    #Add it to the database and commit the changes
+    db.session.add(user)
+    db.session.commit()
+    #Return the user to check the request was successful
+    return jsonify(user_schema.dump(user))
